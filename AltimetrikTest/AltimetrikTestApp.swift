19aa2487d449946e450601b44baefc4e3434d684
Abraham Escamilla Pinelo <abraham_1992@hotmail.com>
2024-06-05 15:33:52 -0600

    Initial Commit

---
+++ AltimetrikTest/AltimetrikTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AltimetrikTestApp.swift
+//  AltimetrikTest
+//
+//  Created by Abraham Escamilla Pinelo on 05/06/24.
+//
+
+import SwiftUI
+
+@main
+struct AltimetrikTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
